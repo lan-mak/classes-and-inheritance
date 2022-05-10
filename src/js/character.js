afb9a3165e1d1845793a196363f890906fa0302b
lan-mak <lan-mak@ya.ru>
2022-05-10 11:53:29 +0300

    ver 0.04

--- src/js/character.js
+++ src/js/character.js
@@ -1,29 +1,27 @@
-const parametersCharacter = {
-  Bowman: '25/25',
-  Swordsman: '40/10',
-  Magician: '10/40',
-  Undead: '25/25',
-  Zombie: '40/10',
-  Daemon: '10/40',
-};
+export default class Character {
+  constructor(name, type) {
+    const typeCharacter = [
+      'Bowman',
+      'Swordsman',
+      'Magician',
+      'Undead',
+      'Zombie',
+      'Daemon',
+    ];
 
-export default function Character(name, type) {
-  // TODO: add logic here
+    if (name.length >= 2 && name.length <= 10) {
+      this.name = name;
+    } else {
+      throw new Error('Имя должно иметь длинну от 2 до 10 знаков');
+    }
 
-  this.type = type;
-  this.health = 100;
-  this.level = 1;
+    if (typeCharacter.includes(type) && typeof type === 'string') {
+      this.type = type;
+    } else {
+      throw new Error('Выбран несуществующий персонаж');
+    }
 
-  if (name.length >= 2 && name.length <= 10) {
-    this.name = name;
-  } else {
-    throw new Error('Имя должно иметь длинну от 2 до 10 знаков');
-  }
-
-  if ((type in parametersCharacter)) {
-    this.attack = Number(parametersCharacter[type].split('/')[0]);
-    this.defence = Number(parametersCharacter[type].split('/')[1]);
-  } else {
-    throw new Error('Неправильный тип');
+    this.health = 100;
+    this.level = 1;
   }
 }
